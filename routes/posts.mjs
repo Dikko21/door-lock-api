bf4d1a567aa82d2ba854d21cb35fc3d20e01075a
dikko21 <diko.ramadany@gmail.com>
2023-07-22 08:25:30 +0700

    fix: status is not send

--- routes/posts.mjs
+++ routes/posts.mjs
@@ -32,7 +32,7 @@ router.get("/log/id/:id", async (req, res) => {
         status: status
     }
     let result = await collectionLog.insertOne(newDocument);
-    res.send(result).status(200);
+    res.status(200).send(result);
 });
 
 // Checking user permisssion
@@ -40,13 +40,13 @@ router.get("/login/id/:id", async (req, res) => {
     let collection = await db.collection("user");
     let query = { id: req.params.id };
     let userData = await collection.findOne(query);
-    if (!userData) res.send("Not found").status(404);
-    else if (userData.username == 'master') res.send({ status: 0 }).status(303);
+    if (!userData) res.status(404).send("Not found");
+    else if (userData.username == 'master') res.status(303).send({ status: 0 });
     else {
         let currentDay = new Date().getDay()
         let currentHour = new Date().getHours()
 
-        if (userData.schedule[0] != currentDay) res.send({ status: 2 }).status(401);
+        if (userData.schedule[0] != currentDay) res.status(401).send({ status: 2 });
         else {
             if (userData.schedule[1] == '1') {
                 if (currentHour > 6 && currentHour < 12) {
@@ -56,7 +56,7 @@ router.get("/login/id/:id", async (req, res) => {
                         loginDate: new Date()
                     }
                     let result = await log.insertOne(newDocument);
-                    res.send({ status: 1, ...result }).status(202);
+                    res.status(202).send({ status: 1, ...result });
                 } else res.status(401).send({ status: 2 });
             } else {
                 if (currentHour > 11 && currentHour < 17) {
@@ -66,7 +66,7 @@ router.get("/login/id/:id", async (req, res) => {
                         loginDate: new Date()
                     }
                     let result = await log.insertOne(newDocument);
-                    res.send({ status: 1, ...result }).status(202);
+                    res.status(202).send({ status: 1, ...result });
                 } else res.status(401).send({ status: 2 });
             }
         }
@@ -78,7 +78,7 @@ router.get("/createUser/username/:username/id/:id/schedule/:schedule", async (re
     let collection = await db.collection("user");
     let newDocument = req.params
     let result = await collection.insertOne(newDocument);
-    res.send(result).status(200);
+    res.send(result);
 });
 
 // Add a new document to the user collection
@@ -92,7 +92,7 @@ router.get("/editUser/username/:username/id/:id/schedule/:schedule", async (req,
         }
     }
     let result = await collection.updateOne(filter, newDocument);
-    res.send(result).status(200);
+    res.send(result);
 });
 
 export default router;
